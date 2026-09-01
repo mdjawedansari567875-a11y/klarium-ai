7f4f8757f0ce6003b761241bd743b446abef0c9a
mdjawedansari567875-a11y <mdjawedansari567875@gmail.com>
2026-09-02 04:59:18 +0530

    Remove LanguageScreen from SettingsStack

--- src/navigation/SettingsStack.js
+++ src/navigation/SettingsStack.js
@@ -3,7 +3,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SettingsScreen from '../screens/main/SettingsScreen';
 import ApiKeyScreen from '../screens/settings/ApiKeyScreen';
 import DeveloperIdeaScreen from '../screens/settings/DeveloperIdeaScreen';
-import LanguageScreen from '../screens/settings/LanguageScreen';
 import PrivacyPolicyScreen from '../screens/settings/PrivacyPolicyScreen';
 import TermsScreen from '../screens/settings/TermsScreen';
 
@@ -15,7 +14,6 @@ export default function SettingsStack() {
       <Stack.Screen name="SettingsHome" component={SettingsScreen} />
       <Stack.Screen name="ApiKeyScreen" component={ApiKeyScreen} />
       <Stack.Screen name="DeveloperIdeaScreen" component={DeveloperIdeaScreen} />
-      <Stack.Screen name="LanguageScreen" component={LanguageScreen} />
       <Stack.Screen name="PrivacyPolicyScreen" component={PrivacyPolicyScreen} />
       <Stack.Screen name="TermsScreen" component={TermsScreen} />
     </Stack.Navigator>
